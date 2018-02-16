a939440afe86fceab5b6d1699b16267518748066
LeenaKap <leena.kapgate@gmail.com>
2018-02-15 22:25:10 -0600

    updating login page

--- src/test/java/com/qa/tests/logintest2.java
+++ src/test/java/com/qa/tests/logintest2.java
@@ -1,5 +1,6 @@
 package com.qa.tests;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class logintest2 {
@@ -7,5 +8,10 @@ public class logintest2 {
 	@Test
 	public void test1(){
 		System.out.println("this is testcases");
+	}
+	
+	@BeforeTest
+	public void rest2(){
+		System.out.println("running succesfully");
 	}
 }
